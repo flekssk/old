064480c02015b363ac5d3bf00f95e1b276209835
stas <kepamuk@yandex.ru>
2024-02-17 15:26:42 +0300

    auth

---
+++ src/pages/auth/register/RegisterPage.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+import RegisterFormWithIllustration from "../../marketing-ui/register-forms/illustration";
+
+const RegisterPage: FC = function () {
+  return <RegisterFormWithIllustration />;
+};
+
+export default RegisterPage;
