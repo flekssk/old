dc4017bf09df47f80711e74a6ff171a78e88cb5d
stas <kepamuk@yandex.ru>
2024-03-31 13:49:53 +0300

    Revert "charts"
    This reverts commit 533ccbc0

--- src/pages/authentication/vk.tsx
+++ src/pages/authentication/vk.tsx
@@ -25,6 +25,7 @@ export const AuthVk: FC = () => {
       login(location.search);
     }
   }, [location.search]);
+  console.log("🚀 ~ location.search:", location.search);
 
   if (isAuthenticated) {
     return <Navigate to="/" />;
