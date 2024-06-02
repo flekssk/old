9eb87266ef17467e6423907102ebce70b5812ef2
Чуев Максим <VTB4098968@region.vtb.ru>
2024-06-02 13:40:18 +0400

    feat: [SRCDEV-64] Fixes

--- src/pages/dashboard/StatCard.tsx
+++ src/pages/dashboard/StatCard.tsx
@@ -11,9 +11,28 @@ export interface StatCardProps {
     direction?: "up" | "down";
     isPositive: boolean;
   };
+  isCombined?: boolean;
+  secondaryValue?: string;
+  secondaryDiff?: {
+    value: string;
+    percentage?: string;
+    direction?: "up" | "down";
+    isPositive: boolean;
+  };
+  unitForValue?: string;
+  unitForSecondaryValue?: string;
 }
 
-export const StatCard: FC<StatCardProps> = ({ title, value, diff }) => {
+export const StatCard: FC<StatCardProps> = ({
+  title,
+  value,
+  diff,
+  isCombined = false,
+  secondaryValue,
+  secondaryDiff,
+  unitForValue,
+  unitForSecondaryValue,
+}) => {
   return (
     <Card
       theme={{
@@ -21,23 +40,43 @@ export const StatCard: FC<StatCardProps> = ({ title, value, diff }) => {
       }}
     >
       <p className="text-sm">{title}</p>
-      <p className="text-xl font-bold">{value}</p>
-      {diff ? (
+      <p className="text-xl font-bold">
+        {value} {unitForValue ? `${unitForValue}` : ""}
+        {isCombined && secondaryValue
+          ? ` / ${secondaryValue} ${unitForSecondaryValue ? `${unitForSecondaryValue}` : ""}`
+          : ""}
+      </p>
+      {(diff || (isCombined && secondaryDiff)) && (
         <div className="flex items-center gap-1">
-          <span>{diff.value}</span>
-          {diff.direction ? (
+          {diff && (
             <p
               className={`flex items-center gap-1 text-sm ${
                 diff.isPositive ? "text-green-500" : "text-red-500"
               }`}
             >
-              <>{diff.direction === "up" ? <HiArrowUp /> : <HiArrowDown />}</>
-
-              {diff.percentage ? `${diff.percentage}%` : null}
+              {diff.direction === "up" ? <HiArrowUp /> : <HiArrowDown />}
+              {`${diff.value} ${diff.percentage ? `(${diff.percentage}%)` : ""}`}
             </p>
-          ) : null}
+          )}
+          {isCombined && secondaryDiff && (
+            <p
+              className={`flex items-center gap-1 text-sm ${
+                secondaryDiff.isPositive ? "text-green-500" : "text-red-500"
+              }`}
+            >
+              /
+              {secondaryDiff.direction === "up" ? (
+                <HiArrowUp />
+              ) : (
+                <HiArrowDown />
+              )}
+              {`${secondaryDiff.value} ${
+                secondaryDiff.percentage ? `(${secondaryDiff.percentage}%)` : ""
+              }`}
+            </p>
+          )}
         </div>
-      ) : null}
+      )}
     </Card>
   );
 };
--- src/pages/dashboard/StatsDashBoard.tsx
+++ src/pages/dashboard/StatsDashBoard.tsx
@@ -14,20 +14,36 @@ export type DashBoardStatsData = {
   profitability: Omit<StatCardProps, "title">;
   salesCount: Omit<StatCardProps, "title">;
 };
+
 type StatProps = {
   data: DashBoardStatsData;
 };
 
-export const StatsDashBoard = ({ data }: StatProps) => {
-  return (
-    <div className="grid grid-cols-2 gap-2 md:grid-cols-4 2xl:grid-cols-5">
-      <StatCard title="Чистая прибыль" {...data.profit} />
-      <StatCard title="Процент выкупа" {...data.averageRedemption} />
-      <StatCard title="Продажи р." {...data.sale} />
-      <StatCard title="Продажи шт." {...data.salesCount} />
-      <StatCard title="Маржинальность" {...data.profitability} />
-      <StatCard title="ROI" {...data.roi} />
-      <StatCard title="ДРР" {...data.ddr} />
-    </div>
-  );
-};
+export const StatsDashBoard = ({ data }: StatProps) => (
+  <div className="grid grid-cols-2 gap-2 md:grid-cols-4 2xl:grid-cols-5">
+    <StatCard title="Чистая прибыль" {...data.profit} />
+    <StatCard title="Процент выкупа" {...data.averageRedemption} />
+    <StatCard
+      title="Продажи"
+      value={data.sale.value}
+      diff={data.sale.diff}
+      secondaryValue={data.salesCount.value}
+      secondaryDiff={data.salesCount.diff}
+      unitForValue="р"
+      unitForSecondaryValue="шт"
+      isCombined
+    />
+    <StatCard title="Маржинальность" {...data.profitability} />
+    <StatCard title="ROI" {...data.roi} />
+    <StatCard
+      title="ДРР"
+      value={data.ddr.value}
+      diff={data.ddr.diff}
+      secondaryValue={data.advertisingExpenses.value}
+      secondaryDiff={data.advertisingExpenses.diff}
+      unitForValue="%"
+      unitForSecondaryValue="р"
+      isCombined
+    />
+  </div>
+);
